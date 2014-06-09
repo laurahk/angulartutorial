f523c935416971a9151191cff52787eb05e51f02
laurahk <Laura.Howarth-Kirke@bbc.co.uk>
2014-06-09 12:56:38 +0100

    Level 2: Tabs and changing images

--- app.js
+++ app.js
@@ -7,24 +7,55 @@
   // the function is the code run when the StoreController is called
   app.controller('StoreController', function() {
     // properties of the controller
-    this.products = gems;
+    this.products = animals;
+  });
+
+  app.controller('GalleryController', function() {
+    this.currentImage = 0;
+
+    this.setCurrentImage = function(cur) {
+      this.currentImage = cur;
+    };
+  });
+
+  app.controller('TabController', function() {
+    this.currentTab = 1;
+
+    this.setTab = function(tab) {
+      this.currentTab = tab;
+    };
+
+    this.isActiveTab = function(tab) {
+      console.log("hello " + this.currentTab == tab);
+      return this.currentTab == tab;
+    };
   });
 
   // a javascript object of something to sell in store
-  var gems = [
+  var animals = [
     {
-      name: 'Dodecahedron',
-      price: 2.95,
-      description: 'Has 12 faces',
-      canPurchase: true,
-      soldOut: true,
-    },
-    {
-      name: 'Pentagonal Gem',
-      price: 5.95,
-      description: 'Has 5 faces',
+      name: 'Puppy',
+      price: 10,
+      description: 'Cuter than cats',
       canPurchase: true,
       soldOut: false,
+      images: [
+        "images/puppy1.jpg",
+        "images/puppy2.jpg",
+        "images/puppy3.jpeg",
+      ]
+    },
+    {
+      name: 'Kitten',
+      price: 5.95,
+      description: 'You don\'t have to walk them',
+      canPurchase: true,
+      soldOut: false,
+      images: [
+        "images/kitten1.jpg",
+        "images/kitten2.jpg",
+        "images/kitten3.jpg",
+      ]
     },
   ]
 
